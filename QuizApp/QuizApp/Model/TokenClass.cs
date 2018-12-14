3c420d50ececf59c69a2dc81d5fad1b78308f056
lawale <lawale99@gmail.com>
2018-12-14 23:24:26 +0100

    Models Used In the App

---
+++ QuizApp/QuizApp/Model/TokenClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuizApp.Model
+{
+    public class TokenClass
+    {
+        public int ResponseCode { get; set; }
+        public string ResponseMessage { get; set; }
+        public string Token { get; set; }
+    }
+}
---
+++ QuizApp/QuizApp/Model/Trivia.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using QuizApp.Extension;
+
+namespace QuizApp.Model
+{
+    public class Trivia
+    {
+        public int ResponseCode { get; set; }
+        public IList<Result> Results { get; set; }
+    }
+
+    public class Result
+    {
+        public string Category { get; set; }
+        public string Type { get; set; }
+        public string Difficulty { get; set; }
+        public string Question { get; set; }
+        public string CorrectAnswer { get; set; }
+        public string SelectedAnswer { get; set; }
+        public IList<string> IncorrectAnswers { private get; set; }
+
+        /// <summary>
+        /// instantiate correct answer with other answerrs in a list.
+        /// Shuffle list to randomize the position of Correct Answer
+        /// </summary>
+        public IList<string> Options
+        {
+            get
+            {
+                var list = new List<string>(IncorrectAnswers)
+                {
+                    CorrectAnswer
+                };
+                list.ShuffleList();
+                return list;
+            }
+        }
+    }
+}
